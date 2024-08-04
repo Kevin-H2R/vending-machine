8c10d20dfd1b83d2956057bae06a62db87669903
Kevin <herrbach.kevin@gmail.com>
2024-08-05 02:26:35 +0900

    Finishes handling card payment

--- app/InputSection.tsx
+++ app/InputSection.tsx
@@ -10,10 +10,14 @@ const InputSection = () => {
     setCardInserted,
     insertedCash,
     setInsertedCash,
-    products
+    products,
+    setProducts,
+    coinsAvailability,
+    setCoinsAvailability
   } = useMachineContext()
 
   const [errorText, setErrorText] = useState("Welcome")
+  const [processing, setProcessing] = useState(false)
 
   useEffect(() => {
     if (selectedSlot === null) {
@@ -21,28 +25,76 @@ const InputSection = () => {
     }
 
     if (cardInserted) {
-      // next step
       handleCardPayment()
       return
     }
     const diff = products[selectedSlot].price - insertedCash
     if (diff < 0) {
-      handleCashPayment()
+      try {
+        handleCashPayment()
+      } catch (error) {
+
+      }
       return
     }
     setErrorText(`Insert card or ${diff} KRW more`)
 
   }, [cardInserted, insertedCash, selectedSlot, products])
 
+  useEffect(() => {
+    if (processing) {
+      setErrorText("Processing...")
+    }
+  }, [processing])
+
 
   const handleCardPayment = () => {
+    if (!selectedSlot) return
+    setProcessing(true)
     // simulating asking confirmation to the bank with 1sec timeout
     setTimeout(() => {
-
-    }, 1000);
+      setProcessing(false)
+      setProducts(products.map((product, index) => {
+        if (index !== selectedSlot) return product
+        product.quantity -= 1
+        return product
+      }))
+      setSelectedSlot(null)
+      setCardInserted(false)
+      setErrorText("Payment accepted, thank you")
+    }, 3000);
   }
 
   const handleCashPayment = () => {
+    if (!selectedSlot) return
+    setProcessing(true)
+    let diff = insertedCash - products[selectedSlot].price
+    // First intuiton was to use DP, but it is actually overkill in that situation
+    // regarding korean won bills and coins values. Greedy algo will work fine.
+    const coins = [10000, 5000, 1000, 500, 100]
+    const usedCoins = []
+    const availabities = {...coinsAvailability}
+    let found = false
+    while (diff > 0) {
+      console.log(diff)
+      found = false
+      for (let i = 0; i < coins.length; ++i) {
+        console.log(availabities[coins[i]])
+        if (coins[i] <= diff && availabities[coins[i]] > 0) {
+          usedCoins.push(coins[i])
+          --availabities[coins[i]]
+          diff -= coins[i]
+          found = true
+          break
+        }
+      }
+      if (!found) {
+        throw new Error("Impossible to give cash back");
+      }
+    }
+    setCoinsAvailability(availabities)
+    // TODO: add customers coins to the machine availabilities
+    setProcessing(false)
 
   }
 
@@ -51,12 +103,14 @@ const InputSection = () => {
     try {
       if (!value) {
         setErrorText("Welcome")
+        setSelectedSlot(null)
         return
       }
       const number = Number.parseInt(value)
       if (number < products.length && number >= 0) {
         if (products[number].quantity < 1) {
           setErrorText("This product is not available anymore")
+          setSelectedSlot(null)
           return
         }
         setSelectedSlot(number)
@@ -76,24 +130,39 @@ const InputSection = () => {
         {errorText}
       </div>
       <input type="number" placeholder="Product n°" className="text-black"
-        onChange={onNumeroChange}
+        onChange={onNumeroChange} readOnly={processing} value={selectedSlot ?? ''}
         />
+
       <button className={`py-4 px-2 border rounded
-        ${cardInserted ? 'bg-gray-300 border-gray-400 text-gray-600' :
+        ${(cardInserted || processing) ? 'bg-gray-300 border-gray-400 text-gray-600' :
          'bg-white border-gray-700 text-black hover:bg-gray-100'}`}
-         onClick={() => setCardInserted(!cardInserted)}>
+         onClick={() => {
+          if (processing) return
+          setCardInserted(!cardInserted)
+          }}>
           { cardInserted ? 'Remove ' : 'Insert ' }card
       </button>
-      <div className="flex flex-wrap gap-2">
+
+      {!processing && <div className="flex flex-wrap gap-2">
         {[100, 500, 1000, 5000, 10000].map(amount => <CashButton key={'cash_' + amount} amount={amount}/>)}
-      </div>
+      </div>}
+
       <div className="flex items-center">
         <span>Inserted Cash:</span>
         <div className="text-xl font-bold ml-5">{insertedCash} KRW</div>
       </div>
-      <button className="bg-red-400 p-3" onClick={() => setInsertedCash(0)}>
+      <button className="bg-red-400 p-3" onClick={() => {
+        if (processing) return
+        setInsertedCash(0)
+        }}>
         Get my money back
       </button>
+      <div className="flex gap-4">{Object.keys(coinsAvailability).map((coin) => <div
+        key={'coin_' + coin}
+        className="flex"
+      >
+        <span>{coin}</span>:<span className="ml-2">{coinsAvailability[Number.parseInt(coin)]}</span>
+      </div>)}</div>
     </div>
 }
 
--- utils/MachineContext.tsx
+++ utils/MachineContext.tsx
@@ -9,6 +9,9 @@ type MachineContextProps = {
   setSelectedSlot: (numero: number | null) => void
   setInsertedCash: (value: number) => void
   products: ProductSlot[]
+  setProducts: (products: ProductSlot[]) => void
+  coinsAvailability: {[key: number]: number}
+  setCoinsAvailability: (availabities: {[key: number]: number}) => void
 }
 
 const MachineContext = createContext<MachineContextProps | undefined>(undefined)
@@ -17,11 +20,18 @@ const MachineProvider = ({children}: {children: ReactNode}) => {
   const [selectedSlot, setSelectedSlot] = useState<number | null>(null)
   const [cardInserted, setCardInserted] = useState<boolean>(false)
   const [insertedCash, setInsertedCash] = useState<number>(0)
+  const [coinsAvailability, setCoinsAvailability] = useState<{[key: number]: number}>({
+    100: 10,
+    500: 10,
+    1000: 10,
+    5000: 10,
+    10000: 10
+  })
 
   // I hard-coded those, fetching it from a DB would be preferable
   const productList: ProductSlot[] = [
     {name: 'Cola', price: 1100, quantity: 10},
-    {name: 'Water', price: 600, quantity: 0},
+    {name: 'Water', price: 600, quantity: 1},
     {name: 'Coffee', price: 700, quantity: 10},
     {name: 'Cola', price: 1100, quantity: 10},
     {name: 'Coffee', price: 700, quantity: 10},
@@ -33,11 +43,13 @@ const MachineProvider = ({children}: {children: ReactNode}) => {
     {name: 'Water', price: 600, quantity: 10},
   ]
 
-  const [products] = useState(productList)
+
+  const [products, setProducts] = useState(productList)
   return <MachineContext.Provider value={{
     selectedSlot, cardInserted, insertedCash,
     setCardInserted, setSelectedSlot, setInsertedCash,
-    products
+    products, setProducts,
+    coinsAvailability, setCoinsAvailability
   }}>
     {children}
   </MachineContext.Provider>
